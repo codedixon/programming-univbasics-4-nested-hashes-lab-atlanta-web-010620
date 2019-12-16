6a1b316bfa324f171347e13291076f0cb96fe22d
Rashad Dixon <dixoncodes@gmail.com>
2019-12-16 02:48:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,5 +126,5 @@ def adding_to_dennis
         }
      }
      programmer_hash[:dennis_ritchie][:languages] = programmer_hash[:dennis_ritchie][:languages] + "Assembly"
-
+return programmer_hash
 end
